38b6ae2e6fd2fcca473dd228f27070544c74dfe2
Kirill Fomichev <fanatid@ya.ru>
2014-12-15 22:42:51 +0300

    add balance example

---
+++ test/integration/balance.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect
+
+var cclib = require('../../src')
+var helpers = require('../helpers')
+
+
+describe('coloredcoinjs-lib (balance)', function () {
+  var colorDataStorage
+  var colorData
+
+  beforeEach(function () {
+    colorDataStorage = new cclib.ColorDataStorage()
+    colorData = new cclib.ColorData(colorDataStorage)
+  })
+
+  afterEach(function () {
+    colorDataStorage.clear()
+  })
+
+  it('EPOBC', function (done) {
+    var epobc = new cclib.EPOBCColorDefinition.fromDesc(
+      1, 'epobc:b8a402f28f247946df2b765f7e52cfcaf8c0714f71b13ae4f151a973647c5170:0:314325')
+
+    var txId = '694dffbf830e50139c34b80abd20c95f37b1a7e6401be5ef579d6f1f973c6c4c'
+    colorData.getColorValue(txId, 0, epobc, helpers.getTestnetTx, function (error, colorValue) {
+      expect(error).to.be.null
+      expect(colorValue).to.be.instanceof(cclib.ColorValue)
+      expect(colorValue.getValue()).to.be.equal(100000)
+      done()
+    })
+  })
+})
