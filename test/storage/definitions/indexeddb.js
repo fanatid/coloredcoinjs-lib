f1e0c4ab4772334dcdcbd24ed8bc5345c83e190f
Kirill Fomichev <fanatid@ya.ru>
2015-05-16 20:04:04 +0300

    add IndexedDB for ColorDefinitionStorage

---
+++ test/storage/definitions/indexeddb.js
@@ -0,0 +1,12 @@
+/* global describe */
+var random = require('bitcore').crypto.Random
+
+var cclib = require('../../../')
+
+require('./implementation')({
+  describe: describe,
+  StorageCls: cclib.storage.definitions.IndexedDB,
+  storageOpts: {
+    dbName: random.getRandomBuffer(5).toString('hex')
+  }
+})
--- test/storage/definitions/interface.js
+++ test/storage/definitions/interface.js
@@ -3,15 +3,15 @@ var expect = require('chai').expect
 
 var cclib = require('../../../')
 
-describe('storage.definitions.Abstract', function () {
+describe('storage.definitions.Interface', function () {
   var storage
 
   beforeEach(function () {
-    storage = new cclib.storage.definitions.Abstract()
+    storage = new cclib.storage.definitions.Interface()
   })
 
   it('isAvailable', function () {
-    expect(cclib.storage.definitions.Abstract.isAvailable()).to.be.false
+    expect(cclib.storage.definitions.Interface.isAvailable()).to.be.false
   })
 
   it('#resolve', function (done) {
